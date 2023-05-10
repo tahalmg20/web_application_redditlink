574c8bb52e2eae56d959a02b30183547984e3931
MOHAMED LAMGHARI <71509112+tahalmg20@users.noreply.github.com>
2023-05-10 10:59:52 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -11,3 +11,20 @@ show_nukeable_resources() {
   aws-nuke --config nuke-config.yml --dry-run
   undo_assume
 }
+
+
+
+# Ajoute un alias de compte à un compte AWS spécifié
+add_account_alias() {
+  ACCOUNT_ID=$1
+  ACCOUNT_ALIAS=$2
+  assume_team $ACCOUNT_ID
+  aws iam create-account-alias --account-alias "$ACCOUNT_ALIAS"
+  undo_assume
+}
+
+while read ACCOUNT_ID; do
+  ACCOUNT_ALIAS="alias-for-account-$ACCOUNT_ID" # Remplacez par un format d'alias de votre choix
+  add_account_alias "$ACCOUNT_ID" "$ACCOUNT_ALIAS"
+done < accounts.txt
+
