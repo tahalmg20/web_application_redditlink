663a69252097b95b92121a8871b88fbeef777f4d
MOHAMED LAMGHARI <71509112+tahalmg20@users.noreply.github.com>
2023-05-10 11:12:08 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -13,15 +13,21 @@ show_nukeable_resources() {
 }
 
 
-
-# Ajoute un alias de compte à un compte AWS spécifié
 add_account_alias() {
   ACCOUNT_ID=$1
   ACCOUNT_ALIAS=$2
+
+  # Vérifie si l'alias respecte les exigences
+  if [[ ! $ACCOUNT_ALIAS =~ ^[a-z0-9][a-z0-9-]*[a-z0-9]$ ]]; then
+    echo "L'alias de compte ${ACCOUNT_ALIAS} ne respecte pas les exigences. Il doit contenir uniquement des chiffres, des lettres minuscules et des traits d'union, et ne peut pas commencer ou se terminer par un trait d'union."
+    return
+  fi
+
   assume_team $ACCOUNT_ID
   aws iam create-account-alias --account-alias "$ACCOUNT_ALIAS"
   undo_assume
 }
+
 
 while read ACCOUNT_ID; do
   ACCOUNT_ALIAS="alias-for-account-$ACCOUNT_ID" # Remplacez par un format d'alias de votre choix
