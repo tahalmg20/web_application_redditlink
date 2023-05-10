b08dbc487b3e91e5c8feafd0d1c43ac771852c1d
MOHAMED LAMGHARI <71509112+tahalmg20@users.noreply.github.com>
2023-05-10 11:38:28 +0200

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -34,3 +34,76 @@ while read ACCOUNT_ID; do
   add_account_alias "$ACCOUNT_ID" "$ACCOUNT_ALIAS"
 done < accounts.txt
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Delete remaining resources in an account using AWS Config
+delete_remaining_resources() {
+  ACCOUNT_ID=$1
+  REGION="us-east-1"
+  RESOURCE_TYPES=(
+    "AWS::EC2::Instance"
+    "AWS::Lambda::Function"
+    "AWS::S3::Bucket"
+    "AWS::EC2::SecurityGroup"
+    "AWS::Logs::LogGroup"
+    "AWS::CloudTrail::Trail"
+    "AWS::SSM::ManagedInstanceInventory"
+  )
+  assume_team $ACCOUNT_ID
+
+  echo "Region: $REGION"
+  echo "Deleting resources:"
+
+  for RESOURCE_TYPE in "${RESOURCE_TYPES[@]}"; do
+    echo "Resource type: $RESOURCE_TYPE"
+    RESOURCE_IDS=$(aws configservice list-discovered-resources --region "$REGION" --resource-type "$RESOURCE_TYPE" --query 'resourceIdentifiers[*].resourceId' --output text)
+
+    for RESOURCE_ID in $RESOURCE_IDS; do
+      echo "Deleting $RESOURCE_ID"
+      case $RESOURCE_TYPE in
+        "AWS::EC2::Instance")
+          aws ec2 terminate-instances --region "$REGION" --instance-ids "$RESOURCE_ID"
+          ;;
+        "AWS::Lambda::Function")
+          aws lambda delete-function --region "$REGION" --function-name "$RESOURCE_ID"
+          ;;
+        "AWS::S3::Bucket")
+          aws s3api delete-bucket --region "$REGION" --bucket "$RESOURCE_ID"
+          ;;
+        "AWS::EC2::SecurityGroup")
+          aws ec2 delete-security-group --region "$REGION" --group-id "$RESOURCE_ID"
+          ;;
+        "AWS::Logs::LogGroup")
+          aws logs delete-log-group --region "$REGION" --log-group-name "$RESOURCE_ID"
+          ;;
+        "AWS::CloudTrail::Trail")
+          aws cloudtrail delete-trail --region "$REGION" --name "$RESOURCE_ID"
+          ;;
+        "AWS::SSM::ManagedInstanceInventory")
+          aws ssm deregister-managed-instance --region "$REGION" --instance-id "$RESOURCE_ID"
+          ;;
+        *)
+          echo "Unknown resource type: $RESOURCE_TYPE"
+          ;;
+      esac
+    done
+  done
+
+  undo_assume
+}
+
+
